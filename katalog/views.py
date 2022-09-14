68f27cca5cfd7af0465fa162ad4d6eed0eec2400
Amanda Christie Tarigan <mandachristie04@gmail.com>
2022-09-14 16:24:51 +0700

    Implement model-view-template

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def show_catalog(request):
+    data_katalog_item = CatalogItem.objects.all()
+    context = {
+        'list_barang': data_katalog_item,
+        'nama': 'Amanda Christie Tarigan',
+        'NPM' : '2106751322'
+    }
+    return render(request, "katalog.html", context)
