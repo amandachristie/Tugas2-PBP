c96c157181b2e5209aa5e7ace7ccd4da1515f59d
Amanda Christie Tarigan <87993867+amandachristie@users.noreply.github.com>
2022-09-27 18:03:17 +0700

    Update settings.py

--- project_django/settings.py
+++ project_django/settings.py
@@ -39,14 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'crispy_forms',
     'example_app',
     'katalog',
     'mywatchlist',
     'todolist',
 ]
-
-CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
