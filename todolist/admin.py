636dfb957e4b8b21d3c2c5e360bad05ac6053118
Amanda Christie Tarigan <87993867+amandachristie@users.noreply.github.com>
2022-09-27 21:18:21 +0700

    Delete todolist directory

--- todolist/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Task
-
-# Mendaftarkan model Task pada admin panel
-admin.site.register(Task)
-
--- todolist/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-class TaskForm(forms.Form):
-    judul = forms.CharField()
-    deskripsi = forms.CharField(widget=forms.Textarea)
-    
